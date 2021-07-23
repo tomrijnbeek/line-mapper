d8ffc68505b244280ad7bd078124a2077515247a
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-07-23 18:41:29 +0100

    🎨 Implement an ArcDrawer

---
+++ LineMapper.Rendering/Drawers/ArcDrawerExtensions.cs
@@ -0,0 +1,21 @@
+using OpenTK.Mathematics;
+
+namespace LineMapper.Rendering.Drawers
+{
+    static class ArcDrawerExtensions
+    {
+        public static void DrawArc<TVertexParameters>(
+            this IArcDrawer<TVertexParameters> arcDrawer,
+            Vector2 center,
+            float directionStart,
+            float directionEnd,
+            float radius,
+            float lineWidth,
+            TVertexParameters parameters,
+            int numEdges)
+        {
+            arcDrawer.DrawArc(
+                center.X, center.Y, 0, directionStart, directionEnd, radius, lineWidth, parameters, numEdges);
+        }
+    }
+}
--- LineMapper.Rendering/Rendering/CoreDrawers.cs
+++ LineMapper.Rendering/Rendering/CoreDrawers.cs
@@ -1,17 +1,22 @@
 using Bearded.Graphics;
 using Bearded.Graphics.Shapes;
+using LineMapper.Rendering.Drawers;
 
 namespace LineMapper.Rendering.Rendering
 {
     public sealed class CoreDrawers
     {
         public IShapeDrawer2<Color> Primitives { get; }
+        public IArcDrawer<Color> Arcs { get; }
 
         public CoreDrawers(CoreRenderers renderers)
         {
             Primitives = new ShapeDrawer2<ColorVertexData, Color>(
                 renderers.Primitives,
                 (xyz, color) => new ColorVertexData(xyz, color));
-        }
+            Arcs = new ArcDrawer<ColorVertexData, Color>(
+                renderers.Primitives,
+                (xyz, color) => new ColorVertexData(xyz, color));
+;        }
     }
 }
