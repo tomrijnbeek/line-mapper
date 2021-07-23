2a66a4fbbb80923929d53b1c2bc557e64e4ab52a
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-07-23 21:25:27 +0100

    ⬆ Upgrade Bearded.Graphics reference for line width fix

--- LineMapper.Model/Core/Constants.cs
+++ LineMapper.Model/Core/Constants.cs
@@ -4,7 +4,7 @@ namespace LineMapper.Model
 {
     public static class Constants
     {
-        public const float LineWidth = 3;
+        public const float LineWidth = 5;
         public static readonly Unit StationRadius = 10.U();
         public static readonly Unit InnerStationRadius = 7.U();
         public static readonly Unit ArcRadius = StationRadius;
